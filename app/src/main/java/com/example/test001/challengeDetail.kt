8b807fe767e5d769e4e57b33db10b07e51be6730
N4YE <naye020519@gmail.com>
2024-01-26 13:15:00 +0900

    file upload22

---
+++ app/src/main/java/com/example/test001/challengeDetail.kt
@@ -0,0 +1,13 @@
+package com.example.test001
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class challengeDetail {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_menu_detail)
+
+        // Here, you can initialize views and retrieve data to display on the detail page
+}
